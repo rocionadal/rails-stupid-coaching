2ccf0695f1f1b85ea610f57a602784c828be6ef7
rocionadal <rocioonadal@gmail.com>
2020-11-23 12:42:45 +0100

    files updated

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,15 @@
 class QuestionsController < ActionController::Base
-  def question
+  def ask
   end
 
-  def ask
+  def answer
+    @question = params[:question]
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
